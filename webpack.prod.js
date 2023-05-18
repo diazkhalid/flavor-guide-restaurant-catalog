52e40eb56a8410dde884a61855b6a10f9bc1af6e
diazka_ <diazananda02@gmail.com>
2023-05-19 00:21:38 +0800

    move-bunlde-analyzer-to-devmode

--- webpack.prod.js
+++ webpack.prod.js
@@ -2,7 +2,6 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
 const WorkboxWebpackPlugin = require('workbox-webpack-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const common = require('./webpack.common');
 
 module.exports = merge(common, {
@@ -52,6 +51,5 @@ module.exports = merge(common, {
       swSrc: path.resolve(__dirname, 'src/scripts/sw.js'),
       swDest: './sw.bundle.js',
     }),
-    new BundleAnalyzerPlugin(),
   ],
 });
